69974b3450aca00bd7cc990fe95bdf366be64289
kevin <kg1941a@gmail.com>
2022-01-12 12:30:03 -0500

    adds all_products route and controller functionality

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,6 @@
 class ProductsController < ApplicationController
+  def show_all_products
+    all_products = Product.all
+    render json: all_products.as_json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Product.create(name: "gel pencils", price: 2, description: "gel pen but a pencil")
