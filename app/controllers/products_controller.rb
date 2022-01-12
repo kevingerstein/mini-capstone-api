b6c8ffb3035d446fac61a30d76d2660ac8df9a94
kevin <kg1941a@gmail.com>
2022-01-12 12:36:52 -0500

    adds routes for individual items

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,19 @@ class ProductsController < ApplicationController
     all_products = Product.all
     render json: all_products.as_json
   end
+
+  def show_book
+    book = Product.find_by(name: "book")
+    render json: book.as_json
+  end
+
+  def show_crayons
+    crayons = Product.find_by(name: "crayons")
+    render json: crayons.as_json
+  end
+
+  def show_gel_pencils
+    gel_pencils = Product.find_by(name: "gel pencils")
+    render json: gel_pencils.as_json
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   get "/all_products", controller: "products", action: "show_all_products"
+  get "/book", controller: "products", action: "show_book"
+  get "/crayons", controller: "products", action: "show_crayons"
+  get "/gel_pencils", controller: "products", action: "show_gel_pencils"
+
 end
